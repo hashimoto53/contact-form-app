53c29099e8acc63f4ee06894fa45d76e9b3b1b57
hashimoto <silver.stripe.7@hotmail.co.jp>
2026-09-19 21:21:06 +0900

    fix: シーダーとファクトリの性別と名前の連動処理を修正

--- database/factories/ContactFactory.php
+++ database/factories/ContactFactory.php
@@ -11,13 +11,29 @@ class ContactFactory extends Factory
 
     public function definition(): array
     {
+        // 1:男性, 2:女性, 3:その他 を定義通りに生成
+        $gender = $this->faker->numberBetween(1, 3);
+
+        // 性別と完全に連動する日本語のファーストネーム配列
+        $maleNames = ['亮介', '太郎', '大輔', '健太', '翔太', '拓海', '和也', '翼', '京助', '真樹'];
+        $femaleNames = ['桃子', '裕美子', 'さゆり', 'あすか', '陽子', '結衣', '美咲', '千尋', '彩香', '和美'];
+        
+        if ($gender === 1) {
+            $firstName = $this->faker->randomElement($maleNames);
+        } elseif ($gender === 2) {
+            $firstName = $this->faker->randomElement($femaleNames);
+        } else {
+            // その他（3）の場合は男女どちらからでもランダムに選択
+            $firstName = $this->faker->randomElement(array_merge($maleNames, $femaleNames));
+        }
+
         return [
             'category_id' => $this->faker->numberBetween(1, 5),
-            'first_name'  => $this->faker->firstName(),
-            'last_name'   => $this->faker->lastName(),
-            'gender'      => $this->faker->numberBetween(1, 3),
+            'first_name'  => $firstName,
+            'last_name'   => $this->faker->lastName(), // 苗字は共通でランダム
+            'gender'      => $gender,
             'email'       => $this->faker->safeEmail(),
-            'tel'         => $this->faker->numerify('000########'), // ← 数字のみの11桁（例: 00012345678）に変更
+            'tel'         => $this->faker->numerify('090########'), 
             'address'     => $this->faker->address(),
             'building'    => $this->faker->secondaryAddress(),
             'detail'      => $this->faker->realText(50),
--- database/seeders/ContactSeeder.php
+++ database/seeders/ContactSeeder.php
@@ -22,18 +22,35 @@ class ContactSeeder extends Seeder
         $categories = Category::all();
         $tags = Tag::all();
 
+        // 性別と完全に連動する日本語のファーストネーム配列を用意します
+        $maleNames = ['亮介', '太郎', '大輔', '健太', '翔太', '拓海', '和也', '翼', '京助', '真樹'];
+        $femaleNames = ['桃子', '裕美子', 'さゆり', 'あすか', '陽子', '結衣', '美咲', '千尋', '彩香', '和美'];
+
         // 要件通り、20件のダミーデータをループで作成します
         for ($i = 0; $i < 20; $i++) {
+            // 先に性別（1:男性, 2:女性, 3:その他）をランダムに決定
+            $gender = $faker->numberBetween(1, 3);
+
+            // 性別に応じて名前を割り当てる
+            if ($gender === 1) {
+                $firstName = $faker->randomElement($maleNames);
+            } elseif ($gender === 2) {
+                $firstName = $faker->randomElement($femaleNames);
+            } else {
+                // その他の場合は男女どちらからでもランダムに選択
+                $firstName = $faker->randomElement(array_merge($maleNames, $femaleNames));
+            }
+
             // お問い合わせデータを1件作成
             $contact = Contact::create([
                 'category_id' => $categories->random()->id, // 既存のカテゴリからランダム
-                'first_name'  => $faker->firstName,
+                'first_name'  => $firstName,
                 'last_name'   => $faker->lastName,
-                'gender'      => $faker->numberBetween(1, 3), // 1:男性, 2:女性, 3:その他
+                'gender'      => $gender,
                 'email'       => $faker->safeEmail,
-                'tel'         => substr($faker->phoneNumber, 0, 11), // ハイフンなし等に対応できるよう調整
+                'tel'         => $faker->numerify('090########'), // ハイフンなしの11桁の数字に固定
                 'address'     => $faker->prefecture . $faker->city . $faker->streetAddress,
-                'building'    => $faker->optional(0.7)->secondaryAddress, // 70%の確率で建物名を入力（nullable対応）
+                'building'    => $faker->optional(0.7)->secondaryAddress, // 70%の確率で建物名を入力
                 'detail'      => $faker->realText(100), // 120文字以内のテキスト
             ]);
 
